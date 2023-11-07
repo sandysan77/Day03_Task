d388aae80d591b48c656239a1e340e9bace3338c
sandysan77 <santhoshsri84@gmail.com>
2023-11-07 14:18:56 +0530

    odd number array

--- Practice.js
+++ Practice.js
@@ -1,2 +1,14 @@
-let name = 'santhosh'
-console.log(name);
+// Print odd numbers in an array
+
+let oddNumber = function(number){
+    var temp =[];
+        for(var i=0;i<number;i++){
+            if(i%2 !=0){
+                temp.push(i);    
+            }   
+        }
+        return temp;
+}
+
+let result = oddNumber(10);
+console.log(result);
